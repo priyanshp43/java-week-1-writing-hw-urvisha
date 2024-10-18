a23e09be87ed14d798926e8748a559943a5e3bcf
priyanshp43 <priyanshp904@gmail.com>
2024-10-18 19:46:39 +0100

    My Homework is here

---
+++ src/homeworkweek5/Programme_5_Triangle.java
@@ -0,0 +1,26 @@
+package homeworkweek5;
+
+/**
+ * 5. Write a Java Program to print below triangle
+ *           *
+ *          * *
+ *         * * *
+ *        * * * *
+ *       * * * * *
+ *      * * * * * *
+ */
+
+public class Programme_5_Triangle {
+
+        public static void main(String[] args) {
+            System.out.println("       *       ");
+            System.out.println("      * *      ");
+            System.out.println("     * * *     ");
+            System.out.println("    * * * *    ");
+            System.out.println("   * * * * *   ");
+            System.out.println("  * * * * * *  ");
+
+
+        }
+
+    }
